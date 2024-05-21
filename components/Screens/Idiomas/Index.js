165357ef224915ce30aaa9a69e39cd3382feeb25
Raphael Braian <117131921+Raphael-Braian@users.noreply.github.com>
2024-05-21 03:42:18 -0300

    Adicionando Dashboard e Idiomas
    Adicionando as telas de Dashboard e Idiomas com seus respectivos scripts de Index e Style.

---
+++ components/Screens/Idiomas/Index.js
@@ -0,0 +1,17 @@
+import { SafeAreaView, Text, View, Image, TextInput, TouchableOpacity, Button } from 'react-native';
+import Styles from './Styles';
+
+export default function Idiomas({navigation}){
+    return(
+        <View>
+            <Text fontWeight="Bold" fontSize="18">Escolha o Idioma: {"\n"}</Text>
+                <Button style={Styles.button} title='Inglês' onPress={(0)}></Button>
+                    <Text></Text>
+                <Button style={Styles.button} title='Português' onPress={(0)}></Button> 
+                    <Text></Text>
+                <Button style={Styles.button} title='Espanhol' onPress={(0)}></Button>  
+                    <Text></Text>  
+                <Button style={Styles.button} title='Japonês' onPress={(0)}></Button>     
+        </View>
+    )
+}
---
+++ components/Screens/Idiomas/Styles.js
@@ -0,0 +1,63 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '100%',
+  },
+
+  logo: {
+    marginTop: 50,
+    marginBottom: 30,
+
+    width: 230,
+    resizeMode: 'contain',
+  },
+
+  viewInputs:{
+    width: '70%',
+    marginBottom: 20,
+    marginHorizontal: 60,
+  },
+
+  inputs:{
+    marginBottom: 20,
+
+    backgroundColor: 'white',
+    borderRadius: 10,
+    margin: 4,
+    padding: 5,
+  },
+
+  labelInput:{
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    marginLeft: 10
+  },
+
+  button:{
+    backgroundColor: '#0047FF',
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 40,
+    width: '70%',
+    alignItems: 'center'
+  },
+
+  arrowBtn: {
+   position: 'absolute',
+   left: 20,
+   top: 90,
+  }
+
+});
