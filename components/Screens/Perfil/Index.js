cefa17ba31d3d2b64c4be5f667ade5ad557fdbc7
Breno Sales <salesonerb18.22@gmail.com>
2024-06-13 14:26:47 -0300

    UseEffect Perfil

--- components/Screens/Perfil/Index.js
+++ components/Screens/Perfil/Index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { ScrollView, Text, View, Image, FlatList, TouchableOpacity } from 'react-native';
 import Styles from './Styles';
 import Post from '../../Post/Index'
